9fad96e19ceb52da1dbf5c372ed4664d43bb4ce0
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-08 14:44:28 -0700

    Added second page and route to second page.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/result_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -206,13 +207,21 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(
-              top: 10,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ResultsPage()),
+              );
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(
+                top: 10,
+              ),
+              width: double.infinity,
+              height: kBottomContainerHeight,
             ),
-            width: double.infinity,
-            height: kBottomContainerHeight,
           ),
         ],
       ),
