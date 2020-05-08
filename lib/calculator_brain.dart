1957b0ac223dfb347612d20f03c82d64fce1a6f8
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-08 15:46:54 -0700

    Added application logic

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  final int height;
+  final int weight;
+  double _bmi;
+
+  CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    double _bmi = weight / pow(height/100, 2);
+
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResults() {
+    if(_bmi >= 25) {
+      return 'Overweight';
+    }
+    else if (_bmi > 18.5){
+      return 'Normal';
+    }
+    else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if(_bmi >= 25) {
+      return 'You have higher than normal body weight. Try to watch what you eat and exercise more.';
+    }
+    else if (_bmi > 18.5){
+      return 'You have normal body weight.  Good Job!';
+    }
+    else {
+      return 'You have lower than normal body weight.  Try to eat more.';
+    }
+  }
+
+}
